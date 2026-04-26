950f0534bc4d949b6101cce98067a51ff0508be7
himanshu8088 <himanshusoni1234@gmail.com>
2026-04-27 00:26:36 +0530

    Add schema creation to tasty_bytes database setup

--- module-1/hamburg_weather/pipeline/objects/database/tasty_bytes.sql
+++ module-1/hamburg_weather/pipeline/objects/database/tasty_bytes.sql
@@ -1,1 +1,5 @@
 CREATE OR ALTER DATABASE {{env}}_tasty_bytes;
+
+CREATE SCHEMA IF NOT EXISTS {{env}}_tasty_bytes.analytics;
+CREATE SCHEMA IF NOT EXISTS {{env}}_tasty_bytes.harmonized;
+CREATE SCHEMA IF NOT EXISTS {{env}}_tasty_bytes.raw_pos;
